67bf56f0a60a48e7d007bc27b12a74635545224e
raovinnakota <eswarv@sbcglobal.net>
2018-09-17 14:45:33 -0400

    Templates for stream listener

---
+++ templates/db_actions.py
@@ -0,0 +1,5 @@
+from pymongo import MongoClient
+
+#create client with local host port 27017
+client = MongoClient('localhost', 27017)
+ 
---
+++ templates/listener.py
@@ -0,0 +1,54 @@
+from tweepy import Stream
+from tweepy import OAuthHandler
+from tweepy.streaming import StreamListener
+
+ckey = "pF5W6BaHFteEYdkq38cgZ755g"
+csecret = "AwnBHQTmvVr6cMSYKm89vQOXMO3sPyoGvhXMaow3zlg2G74vLq"
+atoken = "2646419088-gtzEn525GUUtTlwpegdZGd8dGLWl8m2IGq9jk9y"
+asecret = "iQbg8ZfJ8KkI06n0lwkCVVprtkzsIp6h1RMXP5LN3Z6o2"
+
+class StreamListener(StreamListener):
+    #This is a class provided by tweepy to access the Twitter Streaming API.
+
+    def on_connect(self):
+        # Called initially to connect to the Streaming API
+        print("You are now connected to the streaming API.")
+
+    def on_error(self, status_code):
+        # On error - if an error occurs, display the error / status code
+        print('An Error has occured: ' + repr(status_code))
+        return False
+
+    def on_data(self, data):
+        print(data)
+        return(True)
+
+auth = OAuthHandler(ckey, csecret)
+auth.set_access_token(atoken, asecret)
+
+twitterStream = Stream(auth, StreamListener())
+twitterStream.filter(track=["#NFL"])
+'''
+    def on_data(self, data):
+        #This is the meat of the script...it connects to your mongoDB and stores the tweet
+        try:
+            client = MongoClient(MONGO_HOST)
+
+            # Use twitterdb database. If it doesn't exist, it will be created.
+            db = client.twitterdb
+
+            # Decode the JSON from Twitter
+            datajson = json.loads(data)
+
+            #grab the 'created_at' data from the Tweet to use for display
+            created_at = datajson['created_at']
+
+            #print out a message to the screen that we have collected a tweet
+            print("Tweet collected at " + str(created_at))
+
+            #insert the data into the mongoDB into a collection called twitter_search
+            #if twitter_search doesn't exist, it will be created.
+            db.twitter_search.insert(datajson)
+        except Exception as e:
+           print(e)
+'''
