bc8aab8a88e7607ac08bcb6a46c56b0493736244
raovinnakota <eswarv@sbcglobal.net>
2019-02-25 13:55:03 -0500

    Testing actual sentiment analysis portion

--- templates/sentiment_test.py
+++ templates/sentiment_test.py
@@ -11,14 +11,23 @@ def gather_tweets(phrase, collection):
     entries = [entry for entry in cursor]
     return(entries)
 
+def strip_tweet(text):
+    text.replace('\n', '')
+
 if __name__ == "__main__":
     config = ConfigParser()
     client = MongoClient(MONGO_HOST)
+    analyzer = SentimentIntensityAnalyzer()
     db = client.housedb
     presort = db.presort
     entries = gather_tweets("Donald Trump", presort)
+    #f = open('trump_sentiment.csv', 'w+')
+    #f.write('Tweet, Pos, Neu, Com,\n')
     for i in entries:
-        print(i['text'])
+        tweet = strip_tweet(i['text'])
+        sentiment = analyzer.polarity_scores(sentence)
+        print(tweet, sentiment)
+
     #senate = section_to_dict('HOUSE', config)
     #f = open('house_count.csv', 'w+')
     #f.write('Race,Tweets\n')
