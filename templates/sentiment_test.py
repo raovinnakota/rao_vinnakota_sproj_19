67ec2e3046bd2fd1f01ba06abef5b0fcfb9bd039
raovinnakota <eswarv@sbcglobal.net>
2019-02-25 12:59:40 -0500

    Testing Sentiment

---
+++ templates/sentiment_test.py
@@ -0,0 +1,27 @@
+import ast
+from configparser import ConfigParser
+from pymongo import MongoClient
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
+def gather_tweets(phrase, collection):
+    cursor = collection.find({"text": {"$regex" : phrase, "$options": "$i"}})
+    entries = cursor[:]
+    return(entries)
+
+
+if __name__ == "__main__":
+    config = ConfigParser()
+    client = MongoClient(MONGO_HOST)
+    db = client.housedb
+    presort = db.presort
+    entries = gather_tweets("Donald Trump", presort)
+    print(len(entries))
+    #senate = section_to_dict('HOUSE', config)
+    #f = open('house_count.csv', 'w+')
+    #f.write('Race,Tweets\n')
+    #for i in senate:
+    #    print(i)
+    #    count = gather_tweets(i, presort)
+    #    print(count)
+    #    f.write(i + ',' + str(count) + '\n')
+    #f.close()
