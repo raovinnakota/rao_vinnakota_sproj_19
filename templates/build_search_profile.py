6f6d44fc5f6c79674cceb7d625cc26e64d746aa5
raovinnakota <eswarv@sbcglobal.net>
2019-03-08 10:45:07 -0500

    Writing code to build a sentiment profile for each search term

--- templates/build_search_profile.py
+++ templates/build_search_profile.py
@@ -5,8 +5,17 @@ from pymongo import MongoClient
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
 MONGO_HOST = 'mongodb://localhost/'
-POLITICIAN = sys.argv[1]
-DB = sys.argv[2]
+#POLITICIAN = sys.argv[1]
+#DB = sys.argv[2]
+
+def section_to_dict(section, parser):
+    #change path to your ini file if running locally
+    parser.read(FILE_NAME)
+    out_dict = {}
+    for key in parser[section]:
+        temp = ast.literal_eval(parser[section][key])
+        out_dict[temp[-1]] = temp[:-1]
+    return(out_dict)
 
 def gather_tweets(phrase, collection):
     cursor = collection.find({"text": {"$regex" : phrase, "$options": "$i"}})
@@ -17,20 +26,19 @@ def strip_tweet(text):
     text = text.replace('\n', '')
     return(text)
 
-if __name__ == "__main__":
-    config = ConfigParser()
+def build_sentiment_profile(politician, db_choice):
     client = MongoClient(MONGO_HOST)
     analyzer = SentimentIntensityAnalyzer()
-    if sys.argv[2] == 'house':
+    if db_choice == 'house':
         db = client.housedb
         presort = db.presort
-    if sys.argv[2] == 'senate':
+    if db_choice == 'senate':
         db = client.senatedb
         presort = db.presort
-    if sys.argv[2] == 'misc':
+    if db_choice == 'misc':
         db = client.miscdb
         presort = db.misc
-    entries = gather_tweets(POLITICIAN, presort)
+    entries = gather_tweets(politician, presort)
     ave_score = 0
     pos_tweets = 0
     neg_tweets = 0
@@ -47,8 +55,11 @@ if __name__ == "__main__":
             neg_tweets += 1
         ave_score += sentiment['compound']
         count += 1
-    if count == 0:
-        print("No Tweets for this Politician exist in this database")
-    else:
-        print("Average score:", ave_score/count, "Positive Tweets:", pos_tweets,
-                "Negative Tweets:", neg_tweets, "Neutral Tweets:", neu_tweets)
+    return (ave_score, pos_tweets, neg_tweets, neu_tweets, count)
+
+if __name__ == "__main__":
+    config = ConfigParser()
+    senate = section_to_dict(SENATE)
+    f = open('senate_profile.csv')
+    f.write('Search Term,Pos Tweets,Neg Tweets,Neu Tweets,Average Compound\n')
+    for i in senate:
