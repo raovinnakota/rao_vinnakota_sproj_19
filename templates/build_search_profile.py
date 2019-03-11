dfde70ee606e9e7a0cc0d8848fdb5020114cecdf
raovinnakota <eswarv@sbcglobal.net>
2019-03-11 13:49:39 -0400

    Changed the profile builder

--- templates/build_search_profile.py
+++ templates/build_search_profile.py
@@ -8,7 +8,7 @@ MONGO_HOST = 'mongodb://localhost/'
 FILE_NAME = 'races.ini'
 keywords = ["#midterms", "#vote", "#bluewave", "#redwave", "#democrats", "#elections", "#gop", "#usa"]
 #POLITICIAN = sys.argv[1]
-#DB = sys.argv[2]
+DB = sys.argv[1]
 
 def section_to_dict(section, parser):
     #change path to your ini file if running locally
@@ -61,28 +61,39 @@ def build_sentiment_profile(politician, db_choice):
 
 if __name__ == "__main__":
     config = ConfigParser()
-    #senate = section_to_dict('SENATE', config)
-    senate = section_to_dict('HOUSE', config)
-    #f = open('senate_profile.csv', 'w+')
-    #f = open('misc_profile.csv', 'w+')
-    f = open('house_profile.csv', 'w+')
-    f.write('Search Term,Race,Count,Pos Tweets,Neg Tweets,Neu Tweets,Average Compound\n')
-    #f.write('Search Term,Count,Pos Tweets,Neg Tweets,Neu Tweets,Average Compound\n')
-    '''
-    for i in keywords:
-        print(i)
-        ave_score, pos_tweets, neg_tweets, neu_tweets, count = build_sentiment_profile(i, 'senate')
-        line = str(i) + ',' + str(count) + ',' + str(pos_tweets) + ',' + str(neg_tweets) + ',' + str(neu_tweets) + ',' + str(ave_score/count) + '\n'
-        f.write(line)
-    f.close()
-    '''
-    for i in senate:
-        print(i)
-        for term in senate[i]:
+    if DB == 'senate':
+        senate = section_to_dict('SENATE', config)
+        f = open('senate_profile.csv', 'w+')
+        f.write('Search Term,Race,Count,Pos Tweets,Neg Tweets,Neu Tweets,Average Compound\n')
+        for i in senate:
+            print(i)
+            for term in senate[i]:
+                ave_score, pos_tweets, neg_tweets, neu_tweets, count = build_sentiment_profile(term, 'senate')
+                if (count > 0):
+                    line = str(term) + ',' + str(i) + ',' + str(count) + ',' + str(pos_tweets) + ',' + str(neg_tweets) + ',' + str(neu_tweets) + ',' + str(ave_score/count) + '\n'
+                else:
+                    line = str(term) + ',' + str(i) + ",0,0,0,0,0\n"
+                f.write(line)
+    if DB == 'house':
+        senate = section_to_dict('HOUSE', config)
+        f = open('house_profile.csv', 'w+')
+        f.write('Search Term,Race,Count,Pos Tweets,Neg Tweets,Neu Tweets,Average Compound\n')
+        for i in senate:
+            print(i)
+            for term in senate[i]:
+                ave_score, pos_tweets, neg_tweets, neu_tweets, count = build_sentiment_profile(term, 'senate')
+                if (count > 0):
+                    line = str(term) + ',' + str(i) + ',' + str(count) + ',' + str(pos_tweets) + ',' + str(neg_tweets) + ',' + str(neu_tweets) + ',' + str(ave_score/count) + '\n'
+                else:
+                    line = str(term) + ',' + str(i) + ",0,0,0,0,0\n"
+                f.write(line)
+    if DB == 'misc':
+        f = open('misc_profile.csv', 'w+')
+        f.write('Search Term,Count,Pos Tweets,Neg Tweets,Neu Tweets,Average Compound\n')
+        for i in keywords:
+            print(i)
             ave_score, pos_tweets, neg_tweets, neu_tweets, count = build_sentiment_profile(i, 'senate')
-            if (count > 0):
-                line = str(term) + ',' + str(i) + ',' + str(count) + ',' + str(pos_tweets) + ',' + str(neg_tweets) + ',' + str(neu_tweets) + ',' + str(ave_score/count) + '\n'
-            else:
-                line = str(term) + ',' + str(i) + ",0,0,0,0,0\n"
+            line = str(i) + ',' + str(count) + ',' + str(pos_tweets) + ',' + str(neg_tweets) + ',' + str(neu_tweets) + ',' + str(ave_score/count) + '\n'
             f.write(line)
+        f.close()
     f.close()
