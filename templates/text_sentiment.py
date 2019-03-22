b9a0fca7d79139ff3b078c6ef46dc446c0346c44
raovinnakota <eswarv@sbcglobal.net>
2019-03-22 19:44:30 -0400

    Continuing the program from where it left off

--- templates/text_sentiment.py
+++ templates/text_sentiment.py
@@ -22,10 +22,12 @@ if __name__ == '__main__':
     presort = db.misc
     cursor = presort.find()
     f = open('sentiment.csv', 'w+')
-    f.write("Text,Compound,Pos,Neg,Neu\n")
+    #f.write("Text,Compound,Pos,Neg,Neu\n")
     count = 0
     for i in cursor:
         count += 1
+        while count < 284608:
+            continue
         tweet, sentiment = build_sentiment_profile(i['text'])
         print(count, tweet)
         line = (str(tweet) + ',' + str(sentiment['compound']) +
