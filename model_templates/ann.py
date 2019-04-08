1a8d7ebbe76bc7802f1b43a993a36c18a0dd56cf
raovinnakota <eswarv@sbcglobal.net>
2019-04-08 15:03:27 -0400

    Basic NB algorithm

--- model_templates/ann.py
+++ model_templates/ann.py
@@ -1,9 +1,32 @@
 import pandas as pd
+import numpy as np
+from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import LogisticRegression
 
 FILE_NAME = "senate_profile.csv"
 
+def accuracy(output, answers):
+    right = 0
+    print("Predicted, Actual, Result")
+    for i in range(len(answers)):
+        if (output[i] == answers[i]):
+            right += 1
+            print(output[i], answers[i], "Right")
+        else:
+            print(output[i], answers[i], "Wrong")
+    return(float(right/len(answers)))
+
 senate = pd.read_csv(FILE_NAME)
-train_data = senate.loc[:80, 'Count':'Average Compound']
-train_labels = senate.loc[:80, 'Winner']
-test_data = senate.loc[81:, 'Count':'Average Compound']
-test_labels = senate.loc[81:, 'Winner']
+train_data = np.asarray(senate.loc[:70, 'Count':'Average Compound'])
+train_labels = np.asarray(senate.loc[:70, 'Winner'])
+test_data = np.asarray(senate.loc[71:, 'Count':'Average Compound'])
+test_labels = np.asarray(senate.loc[71:, 'Winner'])
+
+gnb = GaussianNB()
+gnb.fit(train_data, train_labels)
+output = gnb.predict(test_data)
+probs = gnb.predict_proba(test_data)
+print(senate.loc[71:, 'Search Term':'Winner'])
+print(output)
+print(probs)
+print(accuracy(output, test_labels))
